bdfea6243c6066dca87b5cdb1ece3729f1c75ee1
Ankit-2145 <ankitsharma2615@gmail.com>
2025-01-25 22:34:53 +0530

    setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="flex justify-center items-center h-screen border-8 border-red-500">
+      <h1 className="text-8xl text-pink-500 italic tracking-tighter text-balance">
+        Dashboard
+      </h1>
+    </div>
+  );
+};
+
+export default App;
